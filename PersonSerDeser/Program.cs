19a2aa4340b9a3b4441d6a07f55559ba4249d632
Admin <Admin@DESKTOP-047GBOQ>
2023-08-12 13:06:30 +0300

    Добавьте файлы проекта.

---
+++ PersonSerDeser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+Person person = new Person { Name = "John", Age = 30 };
+
+SerializeToXml(person, "person.xml");
+
+Person deserializedPerson = DeserializeFromXml<Person>("person.xml");
+
+Console.WriteLine($"Deserialized Person - Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+
+static void SerializeToXml(object obj, string filename)
+{
+    XmlSerializer serializer = new XmlSerializer(obj.GetType());
+
+    using (TextWriter writer = new StreamWriter(filename))
+    {
+        serializer.Serialize(writer, obj);
+    }
+}
+
+static T DeserializeFromXml<T>(string filename)
+{
+    XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+    using (TextReader reader = new StreamReader(filename))
+    {
+        return (T)serializer.Deserialize(reader);
+    }
+}
